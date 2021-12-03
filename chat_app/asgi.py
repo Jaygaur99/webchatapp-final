8a7f854d47d5a6fd339e2983a69607b29e0a6ff1
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-12-03 14:26:37 +0530

    Done

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat_room.consumers
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- chat_app/settings.py
+++ chat_app/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q^9(es%3uj=r_y)rd1k#%l_1x%5%#4iwre=hy_e5t5(3s)_k@n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -138,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_cdn'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
